6fa33e791d11435fafb2dbb66f58a1ac48a74a8e
SANTHOSHSUNNY123 <pishkekarsanthosh06@gmail.com>
2025-07-30 17:04:44 +0530

    add maven plugin pom.xml

--- src/main/java/com/example/DevopsDemo/DevopsDemoApplication.java
+++ src/main/java/com/example/DevopsDemo/DevopsDemoApplication.java
@@ -2,9 +2,16 @@ package com.example.DevopsDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DevopsDemoApplication {
+
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+	{
+		 return builder.sources(DevopsDemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsDemoApplication.class, args);
